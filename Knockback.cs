72e62e27afe8e71320ff0e58a4b222225ba5a6f8
Joe <69334276+JollyShmo@users.noreply.github.com>
2020-08-07 13:23:42 -0700

    Add files via upload

---
+++ Knockback.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+    public Vector2 movement;
+    public Transform enemy;
+
+    private void Start()
+    {
+        this.GetComponent<Rigidbody2D>();
+    }
+    // May need to change trigger and collider
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == ("Player"))
+        {
+            Knockbac();
+        }
+    }
+  void Knockbac()
+    {
+        float targetDis = Vector2.Distance(transform.position, enemy.position);
+
+        if (targetDis <= 0.6f)
+        {
+            Vector2 direction = enemy.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            direction.Normalize();
+            movement -= direction;
+        }
+    }
+}
