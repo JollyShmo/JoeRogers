653fb7285374c9610a185fde0305e8200baeb4cc
Joe <69334276+JollyShmo@users.noreply.github.com>
2020-08-20 10:33:33 -0700

    Organizing

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed;
+    public Vector2 movement;
+    public Animator enAnim;
+    public GameObject enemy;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        enAnim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        Animation();
+    }
+    private void FixedUpdate()
+    {
+        Radar();
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+    void Animation()
+    {
+        enAnim.SetFloat("Horizontal", movement.x);
+        enAnim.SetFloat("Vertical", movement.y);
+        enAnim.SetFloat("Speed", movement.sqrMagnitude);
+        enAnim.SetBool("moving", true);
+    }
+    void Radar()
+    {
+        float targetDis = Vector2.Distance(transform.position, player.position);
+
+        if (targetDis <= 3f)
+        {
+            Vector2 direction = player.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            direction.Normalize();
+            movement = direction;
+        }
+        if (targetDis <= 0.5f)
+        {
+            Vector2 direction = player.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            direction.Normalize();
+            movement -= direction;
+        }
+    }
+}
---
+++ Scripts/PlayerSpawnRando.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerSpawnRando : MonoBehaviour
+{
+    public Transform[] startingPos;
+
+    void Start()
+    {
+        int randoStartingPos = Random.Range(0, startingPos.Length);
+        transform.position = startingPos[randoStartingPos].position;
+    }
+}
